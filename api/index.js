7e4d3625f075b579c4c07a234c8a26bf010e9803
nitrodal <nichv7@gmail.com>
2024-01-11 23:58:51 -0500

    connect to database/mongoDb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+//testing to make sure we are connected, or else error is thrown. 
+//using env file to protect server info if anyone is seeing this on git, hello! 
+mongoose.connect(process.env.MONGO)
+.then(() => {
+    console.log('You are connected to MongoDb');
+})
+.catch((err) => {
+    console.log("error")
+});
 
 const app = express();
 
