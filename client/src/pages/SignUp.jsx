9bd7f001db314db871872a581eac8dc76ce6e1eb
nitrodal <nichv7@gmail.com>
2024-01-15 20:25:42 -0500

    added sign up page UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -7,7 +7,7 @@ export default function SignUp() {
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
       <form className='flex flex-col gap-4'>
         <input type='text' placeholder='Username' 
-        className = 'border p-3 roundead-lg' id='username' />
+        className = 'border p-3 rounded-lg' id='username' />
         <input type='email' placeholder='Email' 
         className = 'border p-3 rounded-lg' id='email' />
         <input type='password' placeholder='Password' 
