acff8a3107a26d4873f51e7f76e4989e1169294d
nitrodal <nichv7@gmail.com>
2024-01-29 22:58:10 -0500

    create user update api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,6 @@ export default function App() {
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/about' element={<About />} />
-
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
 
